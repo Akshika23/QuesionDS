264c1094677787c4bb1e87cc98b12cc9d52c63c7
Akshika Baghla <68143308+Akshika23@users.noreply.github.com>
2020-09-12 19:42:31 +0530

    Create Question.py

---
+++ Question.py
@@ -0,0 +1,19 @@
+#Assigning elements to different lists
+List1 = ["C","C++","Java"]
+List2 = ["HTML","JavaScript","CSS"]
+List1.append("Python")
+print(List1)
+
+#Accessing elements from a tuple
+Tuple1 = ("Physics","Chemistry","Maths","Biology")
+print(Tuple1[1])
+print(Tuple1[1:3])
+
+#Deleting different dictionary elements
+Dict1 = {"Name":"John",
+        "Class":"7th",
+        "Roll no.":23,
+        "School":"DPS"}
+print(Dict1)
+del Dict1["Class"]
+print(Dict1)
